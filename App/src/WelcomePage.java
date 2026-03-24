d1c5045491a0fd5c7b764cfd90c4e56099787383
Radhitya Varma <radhityavarma2006@gmail.com>
2026-03-24 11:33:27 +0530

    UC1: Welcome Page implemented

---
+++ App/src/WelcomePage.java
@@ -0,0 +1,9 @@
+public class WelcomePage {
+    public static void show() {
+        System.out.println("Welcome to Book My Stay App");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
